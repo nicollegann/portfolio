ae6ed3f3a2050905f845499a0d9c9349c5590c5e
nicollegann <nicollegan@gmail.com>
2022-05-23 22:05:29 +0800

    Add portfolio link

--- components/ProjectPortfolio.js
+++ components/ProjectPortfolio.js
@@ -79,16 +79,16 @@ function ProjectPortfolio() {
                                         <svg className='w-5 h-5' fill='currentColor' viewBox='0 0 20 20' xmlns="http://www.w3.org/2000/svg"><path fillRule='evenodd' d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'></path></svg>  
                                     </button>
                                 </div>
-                                <div className='space-y-6'>
+                                <div className='space-y-6 mb-12'>
                                     <p className='text-base leading-relaxed text-gray-700'>
                                         A personal website built from scratch using NextJS and styled with Tailwind CSS. This site aims to showcase the projects I have worked on, as well as for others to learn more about me as a web developer! 
                                     </p>
                                 </div>
-                                <div className='mt-10'>
-                                    <button type='button' className='project-modal-link-button'>
+                                <div className='flex flex-wrap gap-5'>
+                                    <a href='https://portfolio-nicollegann.vercel.app/' target='_blank' rel='noreferrer' className='project-modal-link-button'>
                                         <SiGithub className='mr-2 text-gray-700' />
                                         Github
-                                    </button>
+                                    </a>
                                 </div>
                             </div>
                         </div>
